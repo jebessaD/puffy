215757ca512ce45d37637670a63b7a905024e890
HP <mintesinotbekele1240@gmail.com>
2025-02-22 14:28:45 +0300

    PUF-2 implementation fo the navbar responsiveness and the hover effect

--- app/components/ui/navbar.tsx
+++ app/components/ui/navbar.tsx
@@ -8,6 +8,7 @@ import { GoPerson } from "react-icons/go";
 import { LiaShoppingBagSolid } from "react-icons/lia";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoIosArrowForward } from "react-icons/io";
+import { navbarMenu } from "@/app/lib/data";
 
 import {
   Sheet,
@@ -15,6 +16,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -30,22 +32,18 @@ export default function Navbar() {
           <Image src="/image/PuffyLogo.png" alt="Puffy Logo" className="sm:hidden -ml-6 -mt-8" width={80} height={80} />
           </SheetTitle>
           <div className="basis-full -mx-6">
+          {
+          navbarMenu.map((item, index) => (
+            <React.Fragment key={index}>
             <hr />
-            <div className="flex justify-between px-3 py-2 items-center">
-            <span>Home</span>
+            <Link href={item.href} className="flex justify-between px-3 py-2 items-center">
+            <span className="text-nowrap">{item.name}</span>
             <IoIosArrowForward />
-            </div>
-            <hr />
-            <div className="flex justify-between px-3 py-2 items-center">
-            <span>Show All</span>
-            <IoIosArrowForward />
-            </div>
-            <hr />
-            <div className="flex justify-between px-3 py-2 items-center">
-            <span>New</span>
-            <IoIosArrowForward />
-            </div>
-            <hr />
+            </Link>
+            </React.Fragment>
+          ))
+          }
+          <hr />
           </div>
           <hr className="-mx-6" />
           <div className="-mx-6 flex justify-around">
@@ -58,18 +56,14 @@ export default function Navbar() {
       </div>
       <Image src="/image/PuffyLogo.png" alt="Puffy Logo" className="max-sm:hidden max-sm:-my-3" width={80} height={80} />
       <ul className="max-sm:hidden h-8 overflow-clip flex justify-center space-x-4 lg:space-x-7 text-xl font-light">
-        <li className="hover:-translate-y-[29px] hover:cursor-pointer transition duration-1000">
-          <div>Home</div>
-          <div>Home</div>
-        </li>
-        <li className="hover:-translate-y-[27px] hover:cursor-pointer transition duration-1000">
-          <div>Shop All</div>
-          <div>Shop All</div>
-        </li>
-        <li className="hover:-translate-y-[29px] hover:cursor-pointer transition duration-1000">
-          <div>New</div>
-          <div>New</div>
-        </li>
+        {
+          navbarMenu.map((item, index) => (
+            <li key={index} className="z-10 hover:-translate-y-[29px] transition duration-1000">
+              <Link href={item.href} className="text-nowrap block">{item.name}</Link>
+              <Link href={item.href} className="text-nowrap block">{item.name}</Link>
+            </li>
+          ))
+        }
       </ul>
       </div>
       <div className="basis-1/3 flex justify-center text-2xl font-medium arsenal-sc-bold">
@@ -77,10 +71,10 @@ export default function Navbar() {
       <Image src="/image/PuffyLogo.png" alt="Puffy Logo" className="sm:hidden -my-3" width={80} height={80} />
       </div>
       <div className="basis-1/3 flex justify-end gap-2 sm:max-lg:gap-4 lg:gap-6">
-      <GoMail className="max-sm:hidden text-3xl cursor-pointer" />
-      <BiLogoInstagram className="max-sm:hidden text-3xl cursor-pointer" />
-      <GoPerson className="max-sm:hidden sm:text-3xl cursor-pointer" />
-      <LiaShoppingBagSolid className="max-sm:hidden sm:text-3xl cursor-pointer" />
+      <GoMail className="hover:scale-125 transition duration-500 max-sm:hidden text-3xl cursor-pointer" />
+      <BiLogoInstagram className="hover:scale-125 transition duration-500 max-sm:hidden text-3xl cursor-pointer" />
+      <GoPerson className="hover:scale-125 transition duration-500 max-sm:hidden sm:text-3xl cursor-pointer" />
+      <LiaShoppingBagSolid className="hover:scale-125 transition duration-500 max-sm:hidden sm:text-3xl cursor-pointer" />
       {/* <GoMail className="text-2xl sm:text-3xl cursor-pointer" /> */}
       </div>
     </div>
