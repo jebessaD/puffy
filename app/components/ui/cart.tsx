77e47322ecde78de3958a3a618d2bc2d6ee3a635
HP <mintesinotbekele1240@gmail.com>
2025-02-26 23:19:49 +0300

    PUF-12 implement-the-cart-section

---
+++ app/components/ui/cart.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { LiaShoppingBagSolid } from "react-icons/lia";
+
+export default function Cart() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <LiaShoppingBagSolid className="hover:scale-125 transition duration-500 text-3xl" />
+      </SheetTrigger>
+      <SheetContent className="max-sm:px-3 max-sm:w-full flex flex-col justify-between">
+        <SheetHeader>
+          <SheetTitle className="text-center">Shopping Cart</SheetTitle>
+          <SheetDescription className="text-center">
+            Click Add to Cart when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <SheetFooter className="justify-center">
+          <SheetClose asChild>
+            <Button type="submit" className="w-full">
+              Add to Cart
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
