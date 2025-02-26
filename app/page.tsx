bdb43a15d9408f79319c8557372d4202fe0c8a52
jebessad <jebessadejene2021@gmail.com>
2025-02-26 16:38:01 +0300

    Improve navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "./components/ui/navbar";
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <div className="w-full h-8 bg-black"></div>
     </div>
   );
